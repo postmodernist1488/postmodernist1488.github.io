2686ca134d60b6c274abc651d6a598442e6641db
postmodernist1488 <elitic.pantheism@gmail.com>
2023-07-24 15:44:01 +0300

    change loading html to a function

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var xhr= new XMLHttpRequest();
-xhr.open('GET', 'sidebar.html', true);
-xhr.onreadystatechange = function() {
-    if (this.readyState!==4) return;
-    if (this.status!==200) return;
-    document.getElementById('sidebar').innerHTML= this.responseText;
-};
-xhr.send();
+function load_html(path, id) {
+    var xhr=new XMLHttpRequest();
+    xhr.open('GET', path, true);
+    xhr.onreadystatechange = function() {
+        if (this.readyState!==4) return;
+        if (this.status!==200) return;
+        document.getElementById(id).innerHTML= this.responseText;
+    };
+    xhr.send();
+}
+load_html('sidebar.html', 'sidebar')
