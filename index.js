970b9212f32117ac24354e2347fe183e4a9f1f84
postmodernist1488 <elitic.pantheism@gmail.com>
2023-07-26 00:26:23 +0300

    a little more content

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-function change_content(path) {
-    var xhr=new XMLHttpRequest();
+function set_content(path) {
+    var xhr = new XMLHttpRequest();
     xhr.open('GET', path, true);
     xhr.onreadystatechange = function() {
         if (this.readyState!==4) return;
         if (this.status!==200) return;
         document.getElementById('contents').innerHTML= this.responseText;
+        scroll(0, 0)
     };
     xhr.send();
 }
-change_content('main.html')
+set_content('main.html')
